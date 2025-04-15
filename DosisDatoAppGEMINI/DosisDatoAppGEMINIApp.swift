5bbb6fd190d7ab34e4f6c110bcd1f979a2a6a23a
Michael Hejmadi <hejmadi@gmail.com>
2025-04-15 16:12:00 +0200

    Initial Commit

---
+++ DosisDatoAppGEMINI/DosisDatoAppGEMINIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DosisDatoAppGEMINIApp.swift
+//  DosisDatoAppGEMINI
+//
+//  Created by Michael Hejmadi on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DosisDatoAppGEMINIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
